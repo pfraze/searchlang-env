43e834fef18e973daa7a70d91bab2f8aaf929d44
Paul Frazee <pfrazee@gmail.com>
2014-07-04 12:54:04 -0500

    added: toggleable editor pane

--- build/main.js
+++ build/main.js
@@ -1,3 +1,27 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-console.log('main.js');
+var $$ = document.querySelector.bind(document);
+var $ = function(selector) {
+	return Array.prototype.slice.call(document.querySelectorAll(selector));
+};
+
+// Editor behaviors
+// ================
+// textarea, behave.js
+var editor = new Behave({
+    textarea:  $$('.editor-textarea'),
+    replaceTab: true,
+    softTabs: true,
+    tabSize: 2,
+    autoOpen: false,
+    overwrite: false,
+    autoStrip: false,
+    autoIndent: false,
+    fence: false
+});
+// open/close toggle button
+$('.btn-toggle-editor').forEach(function($btn) {
+	$btn.addEventListener('click', function() {
+		document.body.classList.toggle('mode-editoropen');
+	});
+});
 },{}]},{},[1])
--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-console.log('main.js');
+var $$ = document.querySelector.bind(document);
+var $ = function(selector) {
+	return Array.prototype.slice.call(document.querySelectorAll(selector));
+};
+
+// Editor behaviors
+// ================
+// textarea, behave.js
+var editor = new Behave({
+    textarea:  $$('.editor-textarea'),
+    replaceTab: true,
+    softTabs: true,
+    tabSize: 2,
+    autoOpen: false,
+    overwrite: false,
+    autoStrip: false,
+    autoIndent: false,
+    fence: false
+});
+// open/close toggle button
+$('.btn-toggle-editor').forEach(function($btn) {
+	$btn.addEventListener('click', function() {
+		document.body.classList.toggle('mode-editoropen');
+	});
+});
