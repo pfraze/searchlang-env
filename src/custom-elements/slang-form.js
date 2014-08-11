1ac2323a63d4659a1ba8a28b9d7f6fa39604dd68
unknown <pfrazee@LAPTOP05.wlion.local>
2014-08-11 11:48:26 -0500

    stripped slangforms to basic behaviors

--- src/custom-elements/slang-form.js
+++ src/custom-elements/slang-form.js
@@ -2,43 +2,21 @@
 var SlangFormPrototype = Object.create(HTMLElement.prototype);
 
 SlangFormPrototype.createdCallback = function() {
-	// Setup attributes
-	if (!this.hasAttribute('method')) {
-		this.setAttribute('method', 'POST');
-	}
-	this.isToplevel = (web.util.findParentNode.byTag(this.parentNode, 'SLANG-FORM') === null);
-
 	// Construct DOM
 	this.innerHTML = [
-		'<div class="slang-form-title">' + (this.getAttribute('title') || '') + '</div>',
-		'<div class="slang-form-body">' + this.innerHTML + '</div>',
-		((this.isToplevel) ?
-			'<div class="slang-form-ctrls"><button>' + this.getAttribute('method') + '</button><button>Close</button></div>'
-			: ''
-		)
+		'<div class="slang-form-title">',
+			(this.getAttribute('title') || ''),
+			'<a href="#" title="Remove">&times;</a>',
+		'</div>',
+		'<div class="slang-form-body">' + this.innerHTML + '</div>'
 	].join('');
 
-	// Add event handlers
-	if (this.isToplevel) {
-		this.lastChild.firstChild.addEventListener('click', onSubmitSlangform.bind(this));
-		this.lastChild.lastChild.addEventListener('click', onCloseSlangform.bind(this));
-		this.addEventListener('keyup', onKeyup.bind(this));
-	}
+	// Add event listeners
+	this.firstChild.lastChild.addEventListener('click', onClickRemove.bind(this));
 };
 
-function onKeyup(e) {
-	// Submit on ctrl-enter
-	if (e.keyCode == 13 && e.ctrlKey) {
-		e.preventDefault();
-		onSubmitSlangform.call(this);
-	}
-}
-
-function onSubmitSlangform() {
-	alert(this.getAttribute('method'));
-}
-
-function onCloseSlangform() {
+function onClickRemove(e) {
+	e.preventDefault();
 	this.parentNode.removeChild(this);
 }
 
